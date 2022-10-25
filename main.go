ec763a5729e15d5e3c98d3790f2e0b114a92bc7c
Matahari Ramadhan <ramadhan.matahari24@gmail.com>
2022-10-25 12:08:24 +0200

    implement create movie

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -47,7 +49,14 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 }
 
 func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000))
+
+	Movies = append(Movies, movie)
+	json.NewEncoder(w).Encode(Movies)
 }
 
 func updateMovie(w http.ResponseWriter, r *http.Request) {
@@ -72,7 +81,7 @@ func main() {
 
 	r.HandleFunc("/", getMovies).Methods("GET")
 	r.HandleFunc("/{id}", getMovie).Methods("GET")
-	r.HandleFunc("/{id}", createMovie).Methods("POST")
+	r.HandleFunc("/", createMovie).Methods("POST")
 	r.HandleFunc("/{id}", updateMovie).Methods("PATCH")
 	r.HandleFunc("/{id}", deleteMovie).Methods("DELETE")
 
